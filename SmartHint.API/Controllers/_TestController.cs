b97c12726f78a20aead4896361f4e801923e5c96
André Luiz Barros <77760155+andre55255@users.noreply.github.com>
2024-07-26 23:29:08 -0300

    feat(log errors): implementa gravação de log de exceção da api.

--- SmartHint.API/Controllers/_TestController.cs
+++ SmartHint.API/Controllers/_TestController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmartHint.Lib.Models.ViewObjects.Utils;
+using SmartHint.Lib.Utils.CustomExceptions;
+using SmartHint.Lib.Utils.Helpers;
+using SmartHint.Lib.Utils.Helpers.LogsApp;
 
 namespace SmartHint.API.Controllers
 {
@@ -7,9 +10,26 @@ namespace SmartHint.API.Controllers
     [Route("test")]
     public class _TestController : ControllerBase
     {
+        private readonly ILogsAppHelper _logAppHelper;
+
+        public _TestController(ILogsAppHelper logAppHelper)
+        {
+            _logAppHelper = logAppHelper;
+        }
+
         [HttpGet]
         public IActionResult GetTest()
         {
+            _logAppHelper.WriteError(new Lib.Models.ViewObjects.Logs.LogErrorVO
+            {
+                Message = "Teste de gravação de log helper",
+                Place = this.GetPlace(),
+                Exception = new ServiceException($"Exceção teste"),
+                Object = new
+                {
+                    teste = "hello world"
+                }
+            });
             return StatusCode(StatusCodes.Status200OK, APIResponseVO.Ok("Hello World!"));
         }
     }
--- SmartHint.Lib.Utils/Helpers/LogsApp/LogsAppHelper.cs
+++ SmartHint.Lib.Utils/Helpers/LogsApp/LogsAppHelper.cs
@@ -1,12 +1,82 @@
-﻿using SmartHint.Lib.Models.ViewObjects.Logs;
+﻿using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using SmartHint.Lib.Models.ViewObjects.Logs;
+using System.Text.Json;
 
 namespace SmartHint.Lib.Utils.Helpers.LogsApp
 {
     public class LogsAppHelper : ILogsAppHelper
     {
+        private readonly IConfiguration _configuration;
+
+        public LogsAppHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void WriteError(LogErrorVO logError)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Task.Run(async () => await FireAndForgetLogRegisterAsync(logError));
+            }
+            catch (Exception) { }
+        }
+
+        private async Task FireAndForgetLogRegisterAsync(LogErrorVO logError)
+        {
+            var connString = GetConnectionString();
+
+            using (var conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    await conn.OpenAsync();
+
+                    var exceptionMessage = logError.Exception != null ? logError.Exception.GetFullTextException() : "";
+
+                    var query =
+                        $@"insert into 
+                            logs_errors(message, place, exception, object) 
+                           values 
+                             (@Message, @Place, @Exception, @Object);";
+
+                    using (var command = new MySqlCommand(query, conn))
+                    {
+                        command.Parameters.AddWithValue("@Message", logError.Message);
+                        command.Parameters.AddWithValue("@Place", logError.Place);
+                        command.Parameters.AddWithValue("@Exception", exceptionMessage);
+                        command.Parameters.AddWithValue("@Object", SerializeObject(logError.Object));
+
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    await conn.CloseAsync();
+                }
+
+            }
+        }
+
+        private string SerializeObject(object obj)
+        {
+            try
+            {
+                return JsonSerializer.Serialize(obj);
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        private string GetConnectionString()
+        {
+            return _configuration.GetConnectionString(ConfigAppSettings.ConnectionStringSmartHintDB);
         }
     }
 }
