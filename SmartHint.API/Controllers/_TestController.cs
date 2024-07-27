a5e785719dc7f67375b2fdb83cdd79df46bb942b
André Luiz Barros <77760155+andre55255@users.noreply.github.com>
2024-07-26 23:32:50 -0300

    fix(_test controller): limpa dados de gravação de log de erros da controller de _test da api.

--- SmartHint.API/Controllers/_TestController.cs
+++ SmartHint.API/Controllers/_TestController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmartHint.Lib.Models.ViewObjects.Utils;
-using SmartHint.Lib.Utils.CustomExceptions;
-using SmartHint.Lib.Utils.Helpers;
-using SmartHint.Lib.Utils.Helpers.LogsApp;
 
 namespace SmartHint.API.Controllers
 {
@@ -10,26 +7,9 @@ namespace SmartHint.API.Controllers
     [Route("test")]
     public class _TestController : ControllerBase
     {
-        private readonly ILogsAppHelper _logAppHelper;
-
-        public _TestController(ILogsAppHelper logAppHelper)
-        {
-            _logAppHelper = logAppHelper;
-        }
-
         [HttpGet]
         public IActionResult GetTest()
         {
-            _logAppHelper.WriteError(new Lib.Models.ViewObjects.Logs.LogErrorVO
-            {
-                Message = "Teste de gravação de log helper",
-                Place = this.GetPlace(),
-                Exception = new ServiceException($"Exceção teste"),
-                Object = new
-                {
-                    teste = "hello world"
-                }
-            });
             return StatusCode(StatusCodes.Status200OK, APIResponseVO.Ok("Hello World!"));
         }
     }
